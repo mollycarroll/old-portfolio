c59d3d45c315238d74539ab3f7445ee95345d378
Molly Carroll <mollyajcarroll@gmail.com>
2021-04-05 07:42:21 -0500

    add react router, add components directory and 3 components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+	HashRouter as Router,
+	Switch,
+	Route
+} from 'react-router-dom'
+import Home from './components/Home'
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+
+        {/* <Route path='/jeopardy'>
+            <Jeopardy />
+        </Route>
+
+        <Route path='/travelist'>
+            <Travelist />
+        </Route>
+
+        <Route path='/gather'>
+            <Gather />
+        </Route>
+
+        <Route path='/serapis'>
+            <Serapis />
+        </Route> */}
+
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useRef, useEffect } from 'react';
+
+function Header() {
+  const [sticky, setSticky] = useState({ isSticky: false, offset: 0 });
+  const headerRef = useRef(null);
+ 
+  // handle scroll event
+  const handleScroll = (elTopOffset, elHeight) => {
+    if (window.pageYOffset > (elTopOffset + elHeight)) {
+      setSticky({ isSticky: true, offset: elHeight });
+    } else {
+      setSticky({ isSticky: false, offset: 0 });
+    }
+  };
+ 
+  // add/remove scroll event listener
+  useEffect(() => {
+    var header = headerRef.current.getBoundingClientRect();
+    const handleScrollEvent = () => {
+      handleScroll(header.top, header.height)
+    }
+ 
+    window.addEventListener('scroll', handleScrollEvent);
+ 
+    return () => {
+      window.removeEventListener('scroll', handleScrollEvent);
+    };
+  }, []);
+
+
+  return (
+    <div style={{ marginTop: sticky.offset }}>
+      <div className="header">
+        <h1>Molly Carroll</h1>
+        <p>software engineer</p>
+      </div>
+ 
+      <div id="sticky-header" className={`navbar${sticky.isSticky ? ' sticky' : ''}`} ref={headerRef}>
+        <a href="#"><button>Home</button></a>
+        <a href="#"><button>About</button></a>
+        <a href="#"><button>Projects</button></a>
+        <a href="#" className="right"><button>Contact</button></a>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
